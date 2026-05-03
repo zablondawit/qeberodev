d7aad8c07fb64355d20e0e86aa54930fc10ef33a
Zablon Dawit <zabjd22@gmail.com>
2026-05-03 19:14:01 +0300

    Refactor article search component and events
    Add quickSearch option to ArticleSearchEvent and include options in
    dispatch
    Switch id-based selectors to class-based selectors for inputs/fieldsets
    and update component CSS
    Remove local debounce.ts and use external debounce package; wire up a
    debounced handler in the articles index
    Remove unused commented script and pass id prop to ArticleSearch
    component

--- apps/web/src/types/article-search.d.ts
+++ apps/web/src/types/article-search.d.ts
@@ -1,9 +1,36 @@
+interface ArticleSearchEventMap extends HTMLElementEventMap {
+    "search-change": ArticleSearchEvent;
+}
+
 declare global {
     declare class ArticleSearchElement extends HTMLElement {
         dispatchSearch(query: string): void;
+
+        addEventListener<K extends keyof ArticleSearchEventMap>(
+            type: K,
+            listener: (
+                this: ArticleSearchElement,
+                ev: ArticleSearchEventMap[K],
+            ) => any,
+            options?: boolean | AddEventListenerOptions,
+        );
+        removeEventListener<K extends keyof ArticleSearchEventMap>(
+            type: K,
+            listener: (
+                this: ArticleSearchElement,
+                ev: ArticleSearchEventMap[K],
+            ) => any,
+            options?: boolean | AddEventListenerOptions,
+        );
     }
+
+    interface ArticleSearchOptions {
+        quickSearch: boolean;
+    }
+
     interface ArticleSearchChangeDetail {
         query: string;
+        options: ArticleSearchOptions;
     }
 
     declare class ArticleSearchEvent extends CustomEvent<ArticleSearchChangeDetail> {}
@@ -12,9 +39,7 @@ declare global {
         "c-article-search": ArticleSearchElement;
     }
 
-    interface GlobalEventHandlersEventMap {
-        "search-change": ArticleSearchEvent;
-    }
+    interface GlobalEventHandlersEventMap {}
 }
 
 export {};
