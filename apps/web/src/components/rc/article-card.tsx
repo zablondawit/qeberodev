58546133c646452207efeff369bfd7a296318bd7
Zablon Dawit <zabjd22@gmail.com>
2026-06-02 11:52:02 +0300

    Add tag badges to article cards

--- apps/web/src/components/rc/article-card.tsx
+++ apps/web/src/components/rc/article-card.tsx
@@ -1,8 +1,29 @@
-import { formatArticleCardDate } from "@/lib/datetime";
-import type { CollectionEntry } from "astro:content";
-import type { FC } from "react";
-import "../../styles/components/article/card.css";
 import "@/components/web-components/article-card";
+import { formatArticleCardDate } from "@/lib/datetime";
+import { cn } from "@/lib/utils";
+import type { CollectionEntry } from "astro:content";
+import type { FC, HTMLAttributes, PropsWithChildren } from "react";
+import "../../styles/components/article/card.css";
+import { type BadgeVariantProps, badgeVariants } from "../ui/badge";
+import type { ClassValue } from "clsx";
+
+type TagProps = {} & BadgeVariantProps & HTMLAttributes<HTMLSpanElement>;
+
+const Badge: FC<PropsWithChildren<TagProps>> = (
+    props = { variant: "default" },
+) => {
+    const { children, variant, className, ...rest } = props;
+
+    return (
+        <span
+            data-role="badge"
+            className={cn(badgeVariants({ variant }), "select-none", className)}
+            {...rest}
+        >
+            {children}
+        </span>
+    );
+};
 
 type ArticleCardProps = {
     article: CollectionEntry<"articles">;
@@ -16,6 +37,12 @@ export const ArticleCard: FC<ArticleCardProps> = ({ article }) => {
                 <a href={`/articles/${article.id}`}>{article.data.title}</a>
             </h3>
             <p className="description">{article.data.description}</p>
+            <span id="article-tags" className="flex flex-row gap-1 pt-2 pb-1">
+                <Badge>{article.data?.category}</Badge>
+                {article.data?.tags.map((tag) => (
+                    <Badge variant={"outline"}>{tag}</Badge>
+                ))}
+            </span>
             <small className="date">{publishedDate}</small>
         </c-article-card>
     );
